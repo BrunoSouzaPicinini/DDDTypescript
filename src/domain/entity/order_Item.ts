e6c433eef9cdd3bd789266d8ef5af76da566b5aa
Bruno de Souza Picinini <brunosouzapicinini@gmail.com>
2023-02-03 00:07:52 -0300

    OrderRepository basic implementations

--- src/domain/entity/order_Item.ts
+++ src/domain/entity/order_Item.ts
@@ -19,6 +19,22 @@ export default class OrderItem {
         return this._quantity;
     }
 
+    get id(): string {
+        return this._id;
+    }
+
+    get name(): string {
+        return this._name;
+    }
+
+    get price(): number {
+        return this._price;
+    }
+
+    get productId(): string {
+        return this._productId;
+    }
+
     orderItemTotal(): number {
         return this._price * this._quantity;
     }
